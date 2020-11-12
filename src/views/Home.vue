9dbf4c02f56ffdc31ddcc826e7f47ebfd4b82a26
Fabio Vansovis Juchem <fabio.juchem@db1.com.br>
2020-11-12 20:44:28 -0300

    Fix dependency

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue';
 import { Vue, Component } from 'vue-property-decorator';
 import PersonService from '@/domain/service/person.service';
 
-@Component({ components: { HelloWorld } })
+@Component({ components: { } })
 export default class Home extends Vue {
   getPerson() {
     PersonService.getPersonByName('Fabio')
