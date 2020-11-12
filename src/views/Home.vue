48ccc874c7ace33b03fe3ed216dd9241f7d59f01
Fabio Vansovis Juchem <fabio.juchem@db1.com.br>
2020-11-12 20:47:24 -0300

    Removed console.log

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,9 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
-import PersonService from '@/domain/service/person.service';
 
 @Component({ components: { } })
 export default class Home extends Vue {
-  getPerson() {
-    PersonService.getPersonByName('Fabio')
-      .then((response: any) => console.log(response));
-  }
 }
 </script>
 <style lang="sass" scoped>
