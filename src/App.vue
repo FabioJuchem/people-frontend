4f43fd9a908cca9793c8924177f189ac5b21ecdc
Fabio Vansovis Juchem <fabio.juchem@db1.com.br>
2020-11-12 22:12:26 -0300

    Done front

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,9 @@
           </span>
         <pre>{{ person }}</pre>
       </div>
+      <v-divider class="mt-14 mb-14"
+        color="black">
+      </v-divider>
       <div>
         <div class="filter">
           <v-text-field
@@ -39,6 +42,94 @@
           </span>
         <pre>{{ personList }}</pre>
       </div>
+      <v-divider class="mt-14 mb-14"
+        color="black">
+      </v-divider>
+      <div class="new-person">
+        <div class="new-person__person">
+          <v-text-field
+            v-model="newPerson.name"
+            :label="'Nome'">
+          </v-text-field>
+          <v-text-field class="ml-10"
+            v-model="newPerson.document"
+            :label="'CPF'">
+          </v-text-field>
+          <v-text-field class="ml-10"
+            v-model="newPerson.birthDate"
+            :label="'Data de nascimento'">
+          </v-text-field>
+        </div>
+          <div v-for="contact in newPerson.contacts" :key="contact" class="new-person__contact">
+            <v-text-field
+              v-model="contact.name"
+              :label="'Nome do contato'">
+            </v-text-field>
+            <v-text-field class="ml-10"
+              v-model="contact.phoneNumber"
+              :label="'Telefone do contato'">
+            </v-text-field>
+            <v-text-field class="ml-10"
+              v-model="contact.email"
+              :label="'email do contato'">
+            </v-text-field>
+          </div>
+        <v-btn
+          @click="saveNewPerson()">
+          Salvar Novo
+        </v-btn>
+          <span class="ml-10">
+            Resultado da Busca
+          </span>
+        <pre>{{ personResponse }}</pre>
+      </div>
+      <v-divider class="mt-14 mb-14"
+        color="black">
+      </v-divider>
+      <div>
+        <div class="filter">
+          <v-text-field
+            v-model="newContact.name"
+            :label="'Nome do contato'">
+          </v-text-field>
+          <v-text-field class="ml-10"
+            v-model="newContact.phoneNumber"
+            :label="'Telefone do contato'">
+          </v-text-field>
+          <v-text-field class="ml-10"
+            v-model="newContact.email"
+            :label="'Email do contato'">
+          </v-text-field>
+        </div>
+        <v-btn
+          @click="saveNewContact()">
+          Salvar Contato por Id
+        </v-btn>
+        <v-text-field class="ml-10"
+          v-model="personId"
+          style="width: 400px"
+          :label="'Id da Pessoa'">
+        </v-text-field>
+          <span class="ml-10">
+            Resultado da Busca
+          </span>
+        <pre>{{ contactResponse }}</pre>
+      </div>
+      <v-divider class="mt-14 mb-14"
+        color="black">
+      </v-divider>
+      <div class="mb-14">
+        <v-btn
+          @click="deletePerson()">
+          Deletar Pessoa por Id
+        </v-btn>
+        <v-text-field class="ml-10"
+          v-model="personToDeleteId"
+          style="width: 400px"
+          :label="'Id da Pessoa a ser deletada'">
+        </v-text-field>
+        <pre>{{ deletionStatus }}</pre>
+      </div>
     </v-main>
   </v-app>
 </template>
@@ -48,6 +139,7 @@ import { Vue, Component } from 'vue-property-decorator';
 import PersonService from '@/domain/service/person.service';
 import Person from './domain/entity/person.entity';
 import Filter from './domain/entity/filter.entity';
+import Contact from './domain/entity/contact.entity';
 
 @Component({ components: { } })
 export default class App extends Vue {
@@ -58,6 +150,24 @@ export default class App extends Vue {
   filter: Filter = Filter.of({});
 
   personList: Person[] = [];
+
+  newPerson: Person = new Person({});
+
+  personResponse: Person = new Person({});
+
+  newContact: Contact = new Contact({});
+
+  contactResponse: Contact = new Contact({});
+
+  personId: string = '';
+
+  deletionStatus: string = '';
+
+  personToDeleteId: string = '';
+
+  created() {
+    this.newPerson.contacts.push(new Contact({}));
+  }
 
   getPersonByName() {
     PersonService.getPersonByName(this.byName)
@@ -72,12 +182,44 @@ export default class App extends Vue {
         this.personList = response;
       });
   }
+
+  saveNewPerson() {
+    PersonService.saveNewPerson(this.newPerson)
+      .then((response: any) => {
+        this.personResponse = response;
+      });
+  }
+
+  saveNewContact() {
+    PersonService.saveNewContact(this.newContact, this.personId)
+      .then((response: any) => {
+        this.contactResponse = response;
+      });
+  }
+
+  deletePerson() {
+    PersonService.deletePerson(this.personToDeleteId)
+      .then(() => {
+        this.deletionStatus = '200';
+      })
+      .catch(() => {
+        this.deletionStatus = 'InternalError';
+      });
+  }
 }
 
 </script>
 
 <style lang="scss" scoped>
 .filter {
+  display: flex;
+}
+
+.new-person__person {
+  display: flex;
+}
+
+.new-person__contact {
   display: flex;
 }
 
--- src/domain/service/person.service.ts
+++ src/domain/service/person.service.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Contact from '../entity/contact.entity';
 import Filter from '../entity/filter.entity';
 import Person from '../entity/person.entity';
 
@@ -12,5 +13,22 @@ export default class PersonService {
     return axios
       .get(`https://fabio-people-api.herokuapp.com/api/v1/person/list?page=0&size=100&name=${filter.name}&document=${filter.document}&birthDate=${filter.birthDate}`)
       .then((response: any) => Person.ofList(response.data.content));
+  }
+
+  public static saveNewPerson(person: Person): Promise<Person> {
+    return axios
+      .post('https://fabio-people-api.herokuapp.com/api/v1/person', person)
+      .then((response: any) => Person.of(response.data));
+  }
+
+  public static saveNewContact(contact: Contact, personId: string): Promise<Contact> {
+    return axios
+      .put(`https://fabio-people-api.herokuapp.com/api/v1/person/${personId}/contact`, contact)
+      .then((response: any) => Contact.of(response.data));
+  }
+
+  public static deletePerson(personId: string): Promise<void> {
+    return axios
+      .delete(`https://fabio-people-api.herokuapp.com/api/v1/person/${personId}`);
   }
 }
