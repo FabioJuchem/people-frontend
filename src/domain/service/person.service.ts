34c0e2eae5ed42bbe7b961666712392289bfda34
Fabio Vansovis Juchem <fabio.juchem@db1.com.br>
2020-11-12 21:10:51 -0300

    Fix port

--- src/domain/service/person.service.ts
+++ src/domain/service/person.service.ts
@@ -4,13 +4,13 @@ import Person from '../entity/person.entity';
 
 export default class PersonService {
   public static getPersonByName(name: string): Promise<Person> {
-    return axios.get(`api/v1/person?name=${name}`)
+    return axios.get(`https://fabio-people-api.herokuapp.com/api/v1/person?name=${name}`)
       .then((response: any) => Person.of(response.data));
   }
 
   public static getAllPerson(filter: Filter): Promise<Person[]> {
     return axios
-      .get(`api/v1/person/list?page=0&size=100&name=${filter.name}&document=${filter.document}&birthDate=${filter.birthDate}`)
+      .get(`https://fabio-people-api.herokuapp.com/api/v1/person/list?page=0&size=100&name=${filter.name}&document=${filter.document}&birthDate=${filter.birthDate}`)
       .then((response: any) => Person.ofList(response.data.content));
   }
 }
